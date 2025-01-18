a2b18d571eaaa4fa29430a87e2d48c5e3c606fe1
Phillipsmart <ezeigwephillip05@gmail.com>
2025-01-18 19:44:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@
 */
 
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['2348187637779']
-global.ownMain = '2348187637779'
+global.owner = ['2348107442282']
+global.ownMain = '2348107442282'
 global.NamaOwner = 'Es Teams Tech' //gausah diganti
 global.sessionName = 'sessionnya'
 global.connect = true // Ubah Ke False Jika Ingin Menggunakan Qr Code
